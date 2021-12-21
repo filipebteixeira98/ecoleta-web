b033f53c51e027358d61135688c2b547528353df
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2021-12-21 13:23:47 -0300

    chore: integrated map to formulary

--- src/pages/CreatePoint/index.tsx
+++ src/pages/CreatePoint/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
+import { MapContainer, TileLayer, Marker } from 'react-leaflet';
 
 import './styles.css';
 
@@ -13,19 +14,19 @@ const CreatePoint = () => {
         <img src={logo} alt="Ecoleta" />
         <Link to="/">
           <FiArrowLeft />
-          Voltar para home
+          Go back
         </Link>
       </header>
       <form>
         <h1>
-          Cadastro do <br /> ponto de coleta
+          Collection point <br /> registration
         </h1>
         <fieldset>
           <legend>
-            <h2>Dados</h2>
+            <h2>Info</h2>
           </legend>
           <div className="field">
-            <label htmlFor="name">Nome da entidade</label>
+            <label htmlFor="name">Entity name</label>
             <input type="text" name="name" id="name" />
           </div>
           <div className="field-group">
@@ -41,57 +42,64 @@ const CreatePoint = () => {
         </fieldset>
         <fieldset>
           <legend>
-            <h2>Endereço</h2>
-            <span>Selecione o endereço no mapa</span>
+            <h2>Address</h2>
+            <span>Select address on map</span>
           </legend>
+          <MapContainer center={[-12.9913412, -38.5165131]} zoom={15}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[-12.9913412, -38.5165131]} />
+          </MapContainer>
           <div className="field-group">
             <div className="field">
-              <label htmlFor="uf">Estado (UF)</label>
+              <label htmlFor="uf">State (UF)</label>
               <select name="uf" id="uf">
-                <option value="0">Selecione uma UF</option>
+                <option value="0">Select a state</option>
               </select>
             </div>
             <div className="field">
-              <label htmlFor="city">Cidade</label>
+              <label htmlFor="city">City</label>
               <select name="city" id="city">
-                <option value="0">Selecione uma cidade</option>
+                <option value="0">Select a city</option>
               </select>
             </div>
           </div>
         </fieldset>
         <fieldset>
           <legend>
-            <h2>Itens de coleta</h2>
-            <span>Selecione um ou mais itens abaixo</span>
+            <h2>Collection items</h2>
+            <span>Select one or more items below</span>
           </legend>
           <ul className="items-grid">
             <li>
               <img src="http://localhost:3333/uploads/oleo.svg" alt="Oleo" />
-              <span>Óleo de cozinha</span>
+              <span>Kitchen oil</span>
             </li>
             <li className="selected">
               <img src="http://localhost:3333/uploads/oleo.svg" alt="Oleo" />
-              <span>Óleo de cozinha</span>
+              <span>Kitchen oil</span>
             </li>
             <li>
               <img src="http://localhost:3333/uploads/oleo.svg" alt="Oleo" />
-              <span>Óleo de cozinha</span>
+              <span>Kitchen oil</span>
             </li>
             <li>
               <img src="http://localhost:3333/uploads/oleo.svg" alt="Oleo" />
-              <span>Óleo de cozinha</span>
+              <span>Kitchen oil</span>
             </li>
             <li>
               <img src="http://localhost:3333/uploads/oleo.svg" alt="Oleo" />
-              <span>Óleo de cozinha</span>
+              <span>Kitchen oil</span>
             </li>
             <li>
               <img src="http://localhost:3333/uploads/oleo.svg" alt="Oleo" />
-              <span>Óleo de cozinha</span>
+              <span>Kitchen oil</span>
             </li>
           </ul>
         </fieldset>
-        <button type="submit">Cadastrar ponto de coleta</button>
+        <button type="submit">Register collection point</button>
       </form>
     </div>
   );
